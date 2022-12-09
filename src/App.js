8da819d3d6f0178dfb122f18280144e767f5c62e
mochadreamboat <jersotmac@gmail.com>
2022-12-09 07:14:01 -0600

    Implemented initial mockup of Item component in App.js.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,20 @@ function App() {
           <li><a href="./about">ABOUT</a></li>
         </ul>
       </header>
+      {/* Mockup for cart / items */}
+      <div className='shopping-cart'>
+        <div className='item'>
+          <img className='item-image' src={require("./images/items/SandevistanMk3.webp")} alt="sandevistan"/>
+          <span className='item-title'>Sandevistan Mk. 3</span>
+          <span className='item-price'>35,000</span>
+          <div className='add-to-cart'>
+          <button>-</button>
+          <input type="number" />
+          <button>+</button>
+          <button className='add'>Add item</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
