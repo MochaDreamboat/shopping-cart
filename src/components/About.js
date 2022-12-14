d9abade7b5a654b0e2662706fe54d41774ee6ad9
mochadreamboat <jersotmac@gmail.com>
2022-12-13 18:40:38 -0600

    Added draft Components for incorporating page routing.

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+// Page describing business
+
+const About = () => {
+    return (
+        <article>
+            <p>Buy some preem chrome here, choom!</p>
+        </article>
+    )
+}
+
+export default About;
---
+++ src/components/Checkout.js
@@ -0,0 +1,12 @@
+// Page for checking out/purchasing items
+
+const Checkout = () => {
+    return (
+    <div id="checkout">
+        <h3>What're ya buyin!?</h3>
+        <h3>What're ya sellin!?</h3>
+    </div>
+    )
+};
+
+export default Checkout;
