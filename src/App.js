a322548dd380fd5820bebba8f8579a7c5f37e856
mochadreamboat <jersotmac@gmail.com>
2022-12-13 19:20:26 -0600

    Moved mockup item out of App

--- src/App.js
+++ src/App.js
@@ -16,19 +16,6 @@ function App() {
         </ul>
       </header>
       {/* Mockup for cart Component / items */}
-      {/* <div className='shopping-cart'>
-        <div className='item'>
-          <img className='item-image' src={require("./images/items/SandevistanMk3.webp")} alt="sandevistan"/>
-          <span className='item-title'>Sandevistan Mk. 3</span>
-          <span className='item-price'>35,000</span>
-          <div className='add-to-cart'>
-            <button>-</button>
-            <input type="number"/>
-            <button>+</button>
-          </div>
-          <button className='add'>Add item</button>
-        </div>
-      </div> */}
       <main>
         <Routes>
           <Route path="/about" element={<About />} />
