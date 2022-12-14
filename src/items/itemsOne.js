9e55d9e83a75bab645bba32815f1f533bda27d5a
mochadreamboat <jersotmac@gmail.com>
2022-12-13 19:54:33 -0600

    Organized buyable items into a JS array (data structure subject to change).

---
+++ src/items/itemsOne.js
@@ -0,0 +1,28 @@
+const mockItems = [
+    {
+      itemName: 'Sandevistan Mk.3 OS',
+      image: 'SandevistanMk3.webp',
+      price: 35000
+    },
+  
+    {
+      itemName: 'Arasaka Mk.4 Cyberdeck',
+      image: 'ArasakaMk4.webp',
+      price: 20000
+    },
+  
+    {
+      itemName: 'Tetratronic Mk.3 Cyberdeck',
+      image: 'TetratronicMk3.webp',
+      price: 10000
+    },
+  
+    {
+      itemName: 'Berserk Mk. 4 OS',
+      image: 'BerserkMk4.webp',
+      price: 25000
+    }
+  
+  ];
+
+export default mockItems;
