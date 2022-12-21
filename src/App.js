4175bc5918b68ac32e86035bc110435c4620b30a
mochadreamboat <jersotmac@gmail.com>
2022-12-20 19:58:12 -0600

    Implemented cart state in app. Adds new item key w/ quantity value in absence of item / updates quantity of present item.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import React, { useEffect, useState } from 'react';
 import { Routes, Route, Link } from "react-router-dom";
 import About from './components/About';
 import Items from './components/Items';
 import Checkout from './components/Checkout.js';
 
 function App() {
+  // Hold cart items in state here.
+  // Items component should be able to update cart state.
+  // Cart state is passed to Cart and Checkout
 
+  const [cart, modifyCart] = useState({});
+
+  const addToCart = (item, quantityToAdd) => {
+    // Look up cart item key inside cart.CartItems
+    // If item not in cart already, add new item to cart items array, stored
+    // as : {itemName: name, itemPrice: price, quantity: }
+    // If already in cartItems, currentQuantity += target value in input.
+    let itemToAdd = item.itemName
+    console.log(itemToAdd)
+    console.log(cart)
+    if (itemToAdd in cart) {
+      modifyCart({...cart, [itemToAdd]: {...cart[itemToAdd], quantity: cart[itemToAdd].quantity + quantityToAdd}});
+      return
+    };
+    modifyCart({...cart, [itemToAdd]: {...cart[itemToAdd], quantity: quantityToAdd}})
+  };
+
+  useEffect( () => {
+    console.table(cart);
+  }, [cart]);
   return (
     <div className="App">
       <header>
@@ -15,11 +39,10 @@ function App() {
           <li><Link to="./checkout">CHECKOUT</Link></li>
         </ul>
       </header>
-      {/* Mockup for cart Component / items */}
       <main>
         <Routes>
           <Route path="/" element={<About />} />
-          <Route path="/items" element={<Items />} />
+          <Route path="/items" element={<Items addToCart={addToCart}/>} />
           <Route path="/checkout" element={<Checkout />}/>
         </Routes>
       </main>
